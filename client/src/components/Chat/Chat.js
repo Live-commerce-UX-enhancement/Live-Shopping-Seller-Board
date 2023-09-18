b1bbae683c5bac50697b5d5bdc38e9a7a0cb57fb
yuseogi0218 <yuseogi0218@gmail.com>
2023-09-18 14:19:17 +0900

    FIX - address

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -12,8 +12,8 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-// const ENDPOINT = 'http://ec2-13-251-142-57.ap-southeast-1.compute.amazonaws.com:5000/';
-const ENDPOINT = 'http://localhost:5000/';
+const ENDPOINT = 'http://ec2-13-251-142-57.ap-southeast-1.compute.amazonaws.com:5000/';
+// const ENDPOINT = 'http://localhost:5000/';
 
 let socket;
 
