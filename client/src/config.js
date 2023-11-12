ba6a9009bc5acc250e768e9e193a3e177ceba7b6
yuseogi0218 <yuseogi0218@gmail.com>
2023-11-12 16:14:40 +0900

    READY DEPLOY

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const API = {
-	NodeJS_URL : "http://ec2-13-251-142-57.ap-southeast-1.compute.amazonaws.com:5001",
+	NodeJS_URL : "http://ec2-13-251-142-57.ap-southeast-1.compute.amazonaws.com:5000",
 
     PythonQA_URL : "http://18.141.54.174:8001"
 };
